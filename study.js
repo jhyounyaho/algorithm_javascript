8ed2944f9f735e718758449b4b0b8c4280d8efc3
jhyounyaho <https://github.com/jhyounyaho>
2020-09-26 21:58:47 +0900

    javascript study

---
+++ study.js
@@ -0,0 +1,231 @@
+// fill() : student 라는 n개원소를 가진 배열에 1로 채워라
+// student = [1, 1, 1, 1]
+let student = new Array(n).fill(1);
+
+// ES6 for ..in : 반복문 객체 순환
+// i = 0, 1, 2, 3
+for (let i in student) {
+  if (student[i] == 2 && student[i - 1] == 0) {
+    student[i] -= 1;
+    student[i - 1] += 1;
+  }
+}
+
+// ES6 for ...of : 반복문 배열 값 탐색
+// s = 1, 1, 1, 1
+for (let s of student) {
+  if (s == 0) {
+    answer--;
+  }
+}
+
+// Math.max() : 최댓값
+let max = Math.max(count1, count2, count3);
+// Math.max.apply(null, []) : 배열에서 최댓값 구하고 싶을 경우
+let max_arr = Math.max.apply(null, [1, 2, 3, 4, 5]);
+
+// includes() : lost 배열에 i+1 이란 값이 있는지 체크
+if (lost.includes(i + 1)) {
+  student[i] -= 1;
+}
+
+/*
+  push() : 배열에 요소 추가
+
+  sort() : 정렬 
+  var score = [4, 11, 2, 10, 3, 1]; 
+  // 오류
+  score.sort(); // 1, 10, 11, 2, 3, 4 
+                // ASCII 문자 순서로 정렬되어 숫자의 크기대로 나오지 않음
+  // 정상 동작 
+  score.sort(function(a, b) { // 오름차순
+      return a - b;
+      // 1, 2, 3, 4, 10, 11
+  });
+  score.sort(function(a, b) { // 내림차순
+      return b - a;
+      // 11, 10, 4, 3, 2, 1
+  });
+
+  slice(start, end) 
+  배열을 얕은 복사 해서 새로운 배열로 반환
+  start 지정하지 않으면 인덱스 0부터 시작
+  end 지정하지 않으면 배열의 길이가 됨 
+  반환 타입은 배열
+
+  var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var arrSlice1 = arr.slice(1);     // [ 2, 3, 4, 5, 6, 7, 8, 9 ]
+  var arrSlice2 = arr.slice(1, 0);  // []
+  var arrSlice3 = arr.slice(1, -1); // [ 2, 3, 4, 5, 6, 7, 8 ]
+  var arrSlice4 = arr.slice(-2);    // [ 8, 9 ]
+
+  splice(start, end) 
+  배열의 기존 요소를 추가, 변경, 삭제해 원본 배열을 변경
+  start는 배열의 변경을 시작할 인덱스
+  반환 타입은 배열로 제거한 요소가 있다면 반환
+
+  var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var arrSplice1 = arr.splice(1, 1); // [ 2 ]
+  var arrSplice2 = arr.splice(1, 3); // [ 3, 4, 5 ]
+  var arrSplice3 = arr.splice(0);    // [ 1, 6, 7, 8, 9 ]
+
+  var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var arrSplice1 = arr.splice(1, 0, '추가1');
+  var arrSplice2 = arr.splice(3, 0, '추가2', '추가3', { add: '추가4' });
+  // [ 1, '추가1', 2, '추가2', '추가3', { add: '추가4' }, 3, 4, 5, 6, 7, 8, 9 ]
+*/
+var answer = [];
+for (var i = 0; i < commands.length; i++) {
+  answer.push(
+    array.slice(commands[i][0] - 1, commands[i][1]).sort((a, b) => a - b)[
+      commands[i][2] - 1
+    ]
+  );
+}
+
+/*
+  sort 로 문자열 n번째 글자 체크해서 정렬하기 
+  오름차순 기준 
+  abce
+  a[0] = a, a[1] = b, a[2] = c, a[3] = e
+*/
+function solution(strings, n) {
+  strings.sort((a, b) => {
+    if (a[n] === b[n]) {
+      return (a > b) - (a < b);
+    } else {
+      return (a[n] > b[n]) - (a[n] < b[n]);
+    }
+  });
+  console.log(strings);
+  return strings;
+}
+
+// join() : 배열 문자열로 변경
+// map() : 배열 반복문
+let result = answer.map((s) => String(s)).join("");
+
+// repeat(n) : n번 반복
+String(j).repeat(4).slice(0, 4) - String(i).repeat(4).slice(0, 4);
+
+// substr(start, length) 문자열 자르기
+// 0 1 2 3 4 순서
+let answer = s.substr(s.length / 2 - 1, 2);
+
+// 짝수 홀수 체크
+if (s.length % 2 === 0) {
+  // 짝홀
+} else {
+  // 홀수
+}
+
+const x = 3.5;
+Math.floor(x); // 내림 3
+Math.ceil(x); // 올림 4
+Math.round(x); // 반올림 4
+
+// 대소관계 없이 주어졌을 경우
+const min = a > b ? b : a;
+const max = a > b ? a : b;
+
+for (let i = min; i <= max; i++) {
+  answer += i;
+}
+
+// 정규식 match 로 문자 찾고, i 로 대소문자 무시, g로 전부 찾는 코드
+// 찾는 문자가 없을 경우 null 인데 바로 length 체크하면 js 에러남 null체크해줘야함
+const p_chk = s.match(/p/gi);
+const p = p_chk !== null ? p_chk.length : 0;
+
+/*
+  문자열 내림차순 
+  split() 문자열 쪼개기
+  sort()로 오름차순 정렬
+  reserve()로 내림차순 정렬
+  join()으로 문자열 합치기
+*/
+const char = s.split("").sort().reverse().join("");
+
+/*
+  parseInt 숫자만 있는지 체크 
+  result = NaN 문자 있을 경우
+  result == inut값  숫자만 있을 경우  
+*/
+console.log(parseInt("a123")); //NaN
+console.log(parseInt("1234")); //1234
+
+/*
+  str into int 형변환 
+  parseInt vs Number 차이 
+  Number : 문자 있으면 무조건 NaN
+  parseInt : 숫자로 시작하는 경우 숫자가 끝날때까지만 형변환
+             숫자로 시작하지 않으면 무조건 NaN
+*/
+console.log(parseInt("1234원")); // 1234
+console.log(parseInt("와1234원")); // NaN
+console.log(Number("1234원")); // NaN
+
+/*
+  아스키코드 
+  대문자(65~90)
+  소문자(97~122)
+
+  String.fromCharCode() 문자로 반환
+  charCodeAt() 아스키코드로 반환
+*/
+for (let i in s) {
+  if (s[i] === " ") {
+    // 문자도 이렇게 체크
+    result += " ";
+  } else {
+    result += String.fromCharCode(
+      s.charCodeAt(i) > 90
+        ? ((s.charCodeAt(i) + n - 97) % 26) + 97
+        : ((s.charCodeAt(i) + n - 65) % 26) + 65
+    );
+  }
+}
+
+/*
+  char.toLowercase()  // 대문자 변환
+  char.toUpperCase    // 소문자 변환 
+*/
+let result = "";
+result += word[i].toLowerCase();
+result += word[i].toUpperCase();
+
+/*
+  map() 
+  배열 내의 모든 요소 각각에 대하여 주어진 함수를 호출한 결과를 모아 
+  새로운 배열을 반환합니다.
+*/
+const int_arr = [1, 2, 3];
+let map_result = int_arr.map((v) => {
+  return v + 1;
+});
+console.log(map_result); // [2, 3, 4]
+
+/*
+  배열의 특정값 찾기 
+  filter()
+  요소들을 모아 새로운 배열로 반환
+
+  find()
+  첫번째 요소의 값을 반환, 없다면 undefined 반환 
+*/
+
+const words = [
+  "spray",
+  "limit",
+  "elite",
+  "exuberant",
+  "destruction",
+  "present",
+];
+const filter_result = words.filter((word) => word.length > 6);
+console.log(filter_result);
+// expected output: Array ["exuberant", "destruction", "present"]
+
+const find_result = words.find((word) => word.length > 6);
+console.log(find_result);
+// expected output: "exuberant"
