c1f2a82964bd3d1b0b1ea57523ffcaa58f70496a
jhyounyaho <https://github.com/jhyounyaho>
2020-11-25 23:36:54 +0900

    프로그래머스 level2 참고할 코드 추가

--- level2_skill_develop.js
+++ level2_skill_develop.js
@@ -106,3 +106,21 @@ function solution(progresses, speeds) {
     }
     return answer;
 }
+
+// 내가 푼 것과 비슷한 풀이 
+function solution(progresses, speeds) {
+    let answer = [0];
+    let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
+    let maxDay = days[0];
+
+    for(let i = 0, j = 0; i< days.length; i++){
+        if(days[i] <= maxDay) {
+            answer[j] += 1;
+        } else {
+            maxDay = days[i];
+            answer[++j] = 1;
+        }
+    }
+
+    return answer;
+}
